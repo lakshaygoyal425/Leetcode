b138f8f004aa15982bdc126fab8693d584cb0eff
Lakshay Goyal <47741067+lakshaygoyal425@users.noreply.github.com>
2021-06-08 12:34:47 +0530

    Delete 1-100 directory

--- 1-100/Reverse Integer.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def reverse(self, x: int) -> int:
-        if x>=0:
-            x=int(str(x)[::-1])
-        else:
-            x=-int(str(-x)[::-1])
-        return x if x<=(2**31-1) and x>=-(2**31) else 0
-        
